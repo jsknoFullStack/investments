d862d4b9e4fe8808c32afd64e3eecb4fc82a1109
jskno <lodmnos@gmail.com>
2018-11-10 13:28:09 +0100

    Starting Back End

---
+++ investments-be/src/main/java/com/jskno/investmentsbe/InvestmentsBeApplication.java
@@ -0,0 +1,12 @@
+package com.jskno.investmentsbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestmentsBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InvestmentsBeApplication.class, args);
+    }
+}
---
+++ investments-be/src/test/java/com/jskno/investmentsbe/InvestmentsBeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jskno.investmentsbe;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InvestmentsBeApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
